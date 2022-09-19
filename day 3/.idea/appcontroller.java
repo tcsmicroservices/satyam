bd50a400fbbf1f9f66687fbb7373fca5cc2ec243
Rama Shanker <rama.shanker@wirelesscar.com>
2022-09-19 08:55:26 +0200

    code clean up

--- day 3/.idea/appcontroller.java
+++
@@ -1,7 +0,0 @@
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-@RestController
-public class AppController{
-    @GetMapping("/hello")
-}
